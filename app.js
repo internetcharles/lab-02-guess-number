af39dcf80efc0b6ab86459a100b1a01787758258
JoLeaper <joleap123@gmail.com>
2020-04-02 15:17:55 -0700

    changed variable names in compareNumbers function

--- app.js
+++ app.js
@@ -1,45 +1,49 @@
 // Get elements
 const button = document.getElementById('button');
-let tries = document.getElementById('tries');
+const resetButton = document.getElementById('reset');
+
+// Gets state
 const input = document.getElementById('user-input');
+
+// Gets derived states
 const tooHigh = document.getElementById('tooHigh');
 const result = document.getElementById('result');
-const gameContainer = document.getElementById('game-container');
 const guessRandom = document.getElementById('guess-random');
+let tries = document.getElementById('tries');
+
+// Gets containers of game info
+const gameContainer = document.getElementById('game-container');
 const hiddenContainer = document.getElementById('hidden-container');
-const resetButton = document.getElementById('reset');
+
 
 let tryCounter = 5;
 tries.textContent = tryCounter;
-let randomNumber = Math.random() * 100;
+let randomNumber = Math.round(Math.random() * 100);
 
 function generateRandom() {
     let randomInput = Math.random() * 100;
     input.value = Math.round(randomInput);
 }
-
 function resetGame() {
     gameContainer.style.display = 'block';
     hiddenContainer.style.display = 'none';
     input.value = 1;
     result.textContent = '';
     tooHigh.textContent = '';
-    randomNumber = Math.random() * 100;
+    randomNumber = Math.round(Math.random() * 100);
+    tries.textContent = 5;
     tryCounter = 5;
 }
-
-
 function compareNumbers() {
+    let guess = input.value;
+    const computerNumber = randomNumber;
 
-    let g = input.value;
-    const c = Math.round(randomNumber);
-
-    if (c > g) {
+    if (computerNumber > guess) {
         tooHigh.textContent = 'Your number is too low!';
         tryCounter--;
         tries.textContent = tryCounter;
     }
-    else if (c < g) {
+    else if (computerNumber < guess) {
         tooHigh.textContent = 'Your number is too high!';
         tryCounter--;
         tries.textContent = tryCounter;
@@ -50,17 +54,13 @@ function compareNumbers() {
         gameContainer.style.display = 'none';
         hiddenContainer.style.display = 'block';
     }
-
     if (tryCounter === 0) {
-        result.textContent = 'YOU LOSE! THE CORRECT NUMBER WAS ' + c;
+        result.textContent = 'YOU LOSE! THE CORRECT NUMBER WAS ' + computerNumber;
         gameContainer.style.display = 'none';
         hiddenContainer.style.display = 'block';
     }
-
-
-
 }
 
 button.addEventListener('click', compareNumbers);
 guessRandom.addEventListener('click', generateRandom); 
-resetButton.addEventListener('click', resetGame); 
+resetButton.addEventListener('click', resetGame);
